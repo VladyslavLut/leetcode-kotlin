b0683abb54f847ec32a36dd9e1d35f7336436851
vladyslav <vladyslav.lutsenko@gmail.com>
2023-09-27 11:35:30 +0300

    13. Roman to Integer - manual map initialization, reverse feed solution, and bool arithmetics instead of if statement

--- src/main/kotlin/RomanToInt.kt
+++ src/main/kotlin/RomanToInt.kt
@@ -1,27 +1,27 @@
+import java.util.HashMap
+
 class RomanToInt {
     class Solution {
-        enum class Roman(val value: Int) {
-            I(1),
-            V(5),
-            X(10),
-            L(50),
-            C(100),
-            D(500),
-            M(1000),
+        private val romans = HashMap<Char, Int>(7).apply {
+            put('I', 1)
+            put('V', 5)
+            put('X', 10)
+            put('L', 50)
+            put('C', 100)
+            put('D', 500)
+            put('M', 1000)
         }
 
         fun romanToInt(s: String): Int {
             var result = 0
             var lastNumber = 0
             for (index in s.indices.reversed()) {
-                val number = Roman.valueOf(s[index].toString()).value
-
-                if (number < lastNumber) {
-                    result -= number
-                } else {
-                    result += number
-                }
-
+                val number = romans[s[index]]!!
+                // true.compareTo(true) -> 0
+                // true.compareTo(false) -> 1
+                // false.compareTo(true) -> -1
+                // false.compareTo(false) -> 0
+                result += number * (number >= lastNumber).compareTo(number < lastNumber)
                 lastNumber = number
             }
             return result
